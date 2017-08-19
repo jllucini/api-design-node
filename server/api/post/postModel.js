5ff6748f5d2e1b052e2a813d48725a106ecad490
Jose Luis Lucini <jllucini@gmail.com>
2017-08-19 14:01:35 +0200

    Schema definition

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -2,6 +2,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  text: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+
+  categories: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'category'
+    }
+  ]
 
 });
 
--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-
+  author: {
+    type: String,
+    ref: 'users',
+    required: true
+  }
 });
 
 module.exports = mongoose.model('user', UserSchema);
